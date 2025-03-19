4cf2d41b614c76ce7a82879a72e4988bf248c23e
Dang123123hai <83021485+Dang123123hai@users.noreply.github.com>
2025-03-19 22:24:58 +1100

    firstcommit

--- src/Cards/Cards.js
+++ src/Cards/Cards.js
@@ -2,12 +2,7 @@ import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
 import chó from '../images/chó.jpg'
-import photo2 from '../images/photo2.jpg'
-import photo3 from '../images/photo3.jpg'
 import photo4 from '../images/photo4.png'
-
-
-
 
 
 function Cards() {
@@ -33,16 +28,6 @@ function Cards() {
               src={photo4}
               text='Luong Hai Dang'
               path='https://www.linkedin.com/in/dang-hai-luong-7a93a6134/'
-            />
-            <CardItem
-              src={photo2}
-              text='Pham Ngoc Quang Vinh'
-              path='https://www.linkedin.com/in/vinh-pham-136b681b1/'
-            />
-            <CardItem
-              src={photo3}
-              text='Le Cong Tuan Minh'
-              path='https://www.linkedin.com/in/tuan-minh-le-cong-243856208/'
             />
           </ul>
         </div>
